a50624bc66f1a6a579d113361747809f7f11b787
Nargiz Gasimbayova <you@example.com>
2022-08-03 17:34:16 +0400

    task 2

---
+++ task2.2/task2.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System
+    namespace task2._2
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string letter = "a";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == letter)
+                {
+                    Console.WriteLine("there is 'A' letter in text");
+                }
+            }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+    }
+}
